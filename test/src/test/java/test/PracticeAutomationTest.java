8d40bbc300c9835f30983ec87fe6606db10bada3
DvoraShapira <0548510016s@gmail.com>
2024-10-27 22:18:45 +0200

    update

--- test/src/test/java/test/PracticeAutomationTest.java
+++ test/src/test/java/test/PracticeAutomationTest.java
@@ -33,29 +33,31 @@ public class PracticeAutomationTest extends BaseUITest {
             Assert.assertEquals(expectedIFrameUrl, driver.getCurrentUrl());
             System.out.println("[Pass:] The iframe url is correct");
 
+            //Switch the driver to first iframe
+            webAction.switchToElemnt(iFramePage.iframe1);
+            //Scroll the iframe to bottom
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", iFramePage.gitHubLink);
+            //wait the btn be clickable
+            waitWebAction.waitForElementToBeEnable(iFramePage.gitHubLink);
+            //Click on the link
+            webAction.clickOnBtn(iFramePage.gitHubLink);
+
+
+            //github Page
+            String expectedUrl = Constans.GITHUB_URL;
+            //Switch the driver to new  git tab
+            helperFunctions.switchToNextTab();
+            try {
+                Assert.assertEquals(expectedUrl, driver.getCurrentUrl());
+                System.out.println("[Pass:] The Github url is correct");
+            } catch (Exception e) {
+                System.out.println("[Fail:] The github link is incorrect");
+            }
+
         } catch (Exception e) {
             System.out.println("[Fail:] The iframe link is incorrect");
         }
-        //Switch the driver to first iframe
-        webAction.switchToElemnt(iFramePage.iframe1);
-        //Scroll the iframe to bottom
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", iFramePage.gitHubLink);
-        //wait the btn be clickable
-        waitWebAction.waitForElementToBeEnable(iFramePage.gitHubLink);
-        //Click on the link
-        webAction.clickOnBtn(iFramePage.gitHubLink);
 
-
-        //github Page
-        String expectedUrl = Constans.GITHUB_URL;
-        //Switch the driver to new  git tab
-        helperFunctions.switchToNextTab();
-        try {
-            Assert.assertEquals(expectedUrl, driver.getCurrentUrl());
-            System.out.println("[Pass:] The Github url is correct");
-        } catch (Exception e) {
-            System.out.println("[Fail:] The github link is incorrect");
-        }
     }
 
 }
